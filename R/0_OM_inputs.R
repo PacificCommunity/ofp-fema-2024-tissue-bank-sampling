84a7b5afacf857be181b935da887948d78f19d58
Tom Peatman <tom.peatman@gmail.com>
2024-11-06 19:24:58 +0000

    extract effort data from frq file

--- R/0_OM_inputs.R
+++ R/0_OM_inputs.R
@@ -43,11 +43,18 @@ vb_pars[vb_pars$sp_code %in% "SKJ", c("L_inf", "k", "t_0")] <- c(86.4, 0.215, -0
 ## Process par.rep file from 2022 skipjack assessment
 ################################################################################
 
+## read in frq file (for effort)
+skj_frq <- read.MFCLFrq("../data/skj_mfcl_files/skj-2022.frq")
+slotNames(skj_frq)
+
+## read in par.rep file
 skj_rep <- read.MFCLRep("../data/skj_mfcl_files/skj-2022-plot-09.par.rep")
 slotNames(skj_rep)
 dimensions(skj_rep)
 
 ## extract quantities of interest
+skj_freq <- freq(skj_frq)
+
 skj_pop_n <- popN(skj_rep)
 skj_sel <- sel(skj_rep)
 skj_q <- q_fishery(skj_rep)
@@ -56,6 +63,7 @@ skj_sd_len <- sd_laa(skj_rep)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## reformat objects for ease of use
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 skj_pop_n <- as.data.frame(skj_pop_n)
 skj_pop_n <- skj_pop_n %>% mutate(., across(where(is.factor), as.character))
@@ -81,10 +89,31 @@ skj_sd_len <- skj_sd_len %>% rename(., qtr = season, value = data)
 skj_sd_len <- skj_sd_len %>% mutate(., qtr = as.numeric(qtr))
 
 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## tidy up skj_sd_len to have consistent structure, where 'age' is age-class in quarters
 
 skj_sd_len <- skj_sd_len %>% mutate(., age_class = (age * 4) + qtr)
 skj_sd_len <- skj_sd_len %>% select(., age_class, value) %>% arrange(., age_class)
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## extract catch and effort data
+
+skj_eff <- skj_freq %>% select(., year, month, fishery, catch, effort) %>% distinct(.)
+skj_eff <- skj_eff %>% mutate(., qtr = ceiling(month / 3)) %>% select(., - month)
+skj_eff <- skj_eff %>% rename(., id_fishery = fishery)
+skj_eff <- skj_eff %>% select(., year, qtr, everything())
+
+## and add 0s for catch and effort
+skj_eff_base <- expand_grid(year = unique(skj_eff$year), qtr = 1:4,
+                            id_fishery = unique(skj_eff$id_fishery))
+
+skj_eff <- skj_eff_base %>%
+    left_join(., skj_eff, by = c("year", "qtr", "id_fishery")) %>%
+    mutate(., across(c(catch, effort), ~ replace_na(.x, 0)))
+
+## add NAs for records missing effort
+skj_eff$effort[skj_eff$effort %in% -1] <- NA
 
 
 ################################################################################
@@ -100,10 +129,12 @@ skj_vb_pars <- vb_pars %>% filter(., sp_code %in% "SKJ")
 saveRDS(skj_lw_pars, file = file.path(data_path, "skj_lw_pars.rds"))
 saveRDS(skj_vb_pars, file = file.path(data_path, "skj_vb_pars.rds"))
 
+saveRDS(skj_eff, file = file.path(data_path, "skj_eff.rds"))
+
+saveRDS(skj_sd_len, file = file.path(data_path, "skj_sd_len.rds"))
 saveRDS(skj_pop_n, file = file.path(data_path, "skj_pop_n.rds"))
 saveRDS(skj_sel, file = file.path(data_path, "skj_sel.rds"))
 saveRDS(skj_q, file = file.path(data_path, "skj_q.rds"))
-saveRDS(skj_sd_len, file = file.path(data_path, "skj_sd_len.rds"))
 
 
 ################################################################################
