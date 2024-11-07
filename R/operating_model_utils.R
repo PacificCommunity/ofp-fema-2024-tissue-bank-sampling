e317606bd171d19e28c8bc795eaf4cf2b1001758
Tom Peatman <tom.peatman@gmail.com>
2024-11-07 20:56:46 +0000

    add fn to implement VB growth

--- R/operating_model_utils.R
+++ R/operating_model_utils.R
@@ -1,3 +1,13 @@
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## functions for use in operating model
+## functions for use in 'operating model'
 
+## estimate mean length at age x with VB growth
+vb_growth <- function(x, L_inf, k, t_0) {
+  stopifnot(all(x >= 0))
+  L_inf * (1 - exp(-k * (x - t_0)))
+}
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## functions for use in 'estimation model' component
+##  - i.e., drawing of catches, and sampling from catches
