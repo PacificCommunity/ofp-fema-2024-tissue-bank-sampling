a9ed9205b7f0fc4488120c23c4ec8f5d86945791
Tom Peatman <tom.peatman@gmail.com>
2024-12-02 14:53:20 +0000

    update TMB VB model - assume SD a log-linear function of mean length at age

--- TMB/fit_vb_growth.cpp
+++ TMB/fit_vb_growth.cpp
@@ -11,7 +11,10 @@ Type objective_function<Type>::operator() ()
   PARAMETER(sigma_a);
   PARAMETER(sigma_b);
 
-  // assume normally distributed errors
-  Type nll = -sum(dnorm(Y, exp(log_L_inf) * (1.0 - exp(- exp(log_k) * (x - t_0))), exp(sigma_a + sigma_b * log(x)), true));
+  // temporary vector holding estimated mean length-at-age
+  vector<Type> y_est = exp(log_L_inf) * (1.0 - exp(- exp(log_k) * (x - t_0)));
+
+  // assume normally distributed, where SD defined by a log-linear function of mean length at age
+  Type nll = -sum(dnorm(Y, y_est, exp(sigma_a + sigma_b * log(y_est)), true));
   return nll;
 }
