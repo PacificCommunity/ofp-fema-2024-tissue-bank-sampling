a68e0edcaa9d5476a157e19f93014a0ef47bd32f
Tom Peatman <tom.peatman@gmail.com>
2024-12-04 09:17:52 +0000

    estimate VB parameters on natural scale (not log-scale)

--- TMB/fit_vb_growth.cpp
+++ TMB/fit_vb_growth.cpp
@@ -5,14 +5,14 @@ Type objective_function<Type>::operator() ()
 {
   DATA_VECTOR(Y);
   DATA_VECTOR(x);
-  PARAMETER(log_L_inf);
-  PARAMETER(log_k);
-  PARAMETER(log_t_0);
+  PARAMETER(L_inf);
+  PARAMETER(k);
+  PARAMETER(t_0);
   PARAMETER(sigma_a);
   PARAMETER(sigma_b);
 
   // temporary vector holding estimated mean length-at-age
-  vector<Type> y_est = exp(log_L_inf) * (1.0 - exp(- exp(log_k) * (x + exp(log_t_0))));
+  vector<Type> y_est = L_inf * (1.0 - exp(- k * (x - t_0)));
 
   // assume normally distributed, where SD defined by a log-linear function of mean length at age
   Type nll = -sum(dnorm(Y, y_est, exp(sigma_a + sigma_b * log(y_est)), true));
